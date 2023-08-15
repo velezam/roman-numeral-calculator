73c5dad9d49fa1c374654ae31efd82ca2c0a0dc9
Amadeo Velez <amadeo.velez@gmail.com>
2023-08-15 13:36:24 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Amadeo Velez 2023
+
+# Solution for the Roman Numeral Calculator for the freeCodeCamp Javascript Algorithms and Data Structures Certification
---
+++ romanNumeralCalculator.js
@@ -0,0 +1,56 @@
+function convertToRoman(num) {
+  let romanNumeral = "";
+
+  while (num > 0) {
+    if (num >= 1000) {
+      romanNumeral += "M";
+      num -= 1000;
+    } else if (num >= 900) {
+      romanNumeral += "CM";
+      num -= 900;
+    } else if (num >= 500) {
+      romanNumeral += "D";
+      num -= 500;
+    } else if (num >= 400) {
+      romanNumeral += "CD";
+      num -= 400;
+    } else if (num >= 100) {
+      romanNumeral += "C";
+      num -= 100;
+    } else if (num >= 90) {
+      romanNumeral += "XC";
+      num -= 90;
+    } else if (num >= 50) {
+      romanNumeral += "L";
+      num -= 50;
+    } else if (num >= 40) {
+      romanNumeral += "XL";
+      num -= 40;
+    } else if (num >= 10) {
+      romanNumeral += "X";
+      num -= 10;
+    } else if (num >= 9) {
+      romanNumeral += "IX";
+      num -= 9;
+    } else if (num >= 5) {
+      romanNumeral += "V";
+      num -= 5;
+    } else if (num >= 4) {
+      romanNumeral += "IV";
+      num -= 4;
+    } else if (num >= 1) {
+      romanNumeral += "I";
+      num -= 1;
+    }
+  }
+
+  return romanNumeral;
+}
+
+convertToRoman(36);
+convertToRoman(4);
+convertToRoman(1002);
+convertToRoman(7898);
+convertToRoman(456);
+convertToRoman(705);
+convertToRoman(109);
